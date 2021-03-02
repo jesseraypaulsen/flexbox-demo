0ae9d48316f15b468752de893f4b4649f59d918c
Jesse Paulsen <jesseraypaulsen@gmail.com>
2021-03-02 15:22:29 -0600

    ...

--- scripts.js
+++ scripts.js
@@ -1,8 +1,25 @@
 const layout = document.querySelector('.container');
-const el = document.getElementById('axis-direction');
-const val = el.value;
 
-el.addEventListener('change', e => {
-  // DOM changes, but does it render?
-  layout.style.flexDirection = e.target.value;
+// https://flaviocopes.com/how-to-uppercase-first-letter-javascript/
+const capitalize = (s) => {
+  if (typeof s !== 'string') return ''
+  return s.charAt(0).toUpperCase() + s.slice(1)
+}
+
+// parse the style attribute strings into the corresponding property from JS/DOM
+const parseAttrIntoProps = (s) => {
+  if (s.includes('-')) {
+    let t = s.split('-');
+    s = t[0].concat(capitalize(t[1]));
+  }
+  return s;
+}
+const ddlists = document.querySelectorAll('select');
+
+ddlists.forEach(list => {
+  let name = list.name;
+  let newName = parseAttrIntoProps(name);
+  list.addEventListener('change', e => {
+    layout.style[newName] = e.target.value;
+  });
 });
