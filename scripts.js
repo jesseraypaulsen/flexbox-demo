3ddbf4ffd101112eda42fef415ba517ebe6d1c6e
Jesse Paulsen <jesseraypaulsen@gmail.com>
2021-03-04 23:23:02 -0600

    ...

--- scripts.js
+++ scripts.js
@@ -12,7 +12,7 @@ const capitalize = (s) => {
 }
 
 // parse the style attribute strings into the corresponding property from JS/DOM
-const parseAttrIntoProps = (s) => {
+const parseAttrIntoProp = (s) => {
   if (s.includes('-')) {
     let t = s.split('-');
     s = t[0].concat(capitalize(t[1]));
@@ -22,29 +22,25 @@ const parseAttrIntoProps = (s) => {
 
 // container controls must be handled differently than item controls
 contControls.forEach(ctrl => {
-  let name = parseAttrIntoProps(ctrl.name);
+  let name = parseAttrIntoProp(ctrl.name);
   ctrl.addEventListener('change', e => {
     layout.style[name] = e.target.value;
   });
 });
 
 itemControls.forEach(ctrl => {
-  let name = parseAttrIntoProps(ctrl.name);
+  let name = parseAttrIntoProp(ctrl.name);
   ctrl.addEventListener('change', e => {
     // applies the change to the div that was clicked last
-    let item = currItems[currItems.length -1];
-    console.log(currItems);
-    console.log(item);
-    document.querySelector(item).style[name] = e.target.value;
+    // let item = currItems[currItems.length -1];
+    // document.querySelector(item).style[name] = e.target.value;
 
     // applies the change to all selected divs
-    // currItems.forEach(item => {
-    //   console.log(document.querySelector(item))
-    //   console.log(e.target.value);
-    //   document.querySelector(item).style[name] = e.target.value;
-    // });
+    currItems.forEach(item => {
+      document.querySelector(item).style[name] = e.target.value;
+    });
   });
-})
+});
 
 document.addEventListener('click', e => {
   if (e.target.classList.contains("box")) {
@@ -57,26 +53,46 @@ document.addEventListener('click', e => {
   }
 });
 
-const spanItems = (items) => {
+// undo selection
+divsToChange.addEventListener('click', e => {
+  let i = currItems.findIndex(val => val === e.target.id);
+  if (i !== -1) {
+    currItems.splice(i, 1);
+    console.log(currItems);
+    divsToChange.removeChild(e.target); // would this step be necessary in MV* framework w/ data-binding?
+  }
+});
+
+//https://www.w3schools.com/jsref/dom_obj_style.asp
+const spanItems = items => {
   removeAllChildNodes(divsToChange);
   return items.map(item => {
     let span = document.createElement('span');
-    span.style.border = "1px solid black"; //https://www.w3schools.com/jsref/dom_obj_style.asp
+    let num = getLastChar(item);
     span.style.backgroundColor = getColor(item);
-    span.innerHTML = item;
+    span.style.padding = "3px";
+    span.innerHTML = num;
+    span.id = item;
     return span;
   })
 }
 
+const getLastChar = item => {
+  let num = item.length - 1;
+  return item.charAt(num);
+}
+
+
 //https://www.javascripttutorial.net/dom/manipulating/remove-all-child-nodes/
-const removeAllChildNodes = (parent) => {
+const removeAllChildNodes = parent => {
   while (parent.firstChild) {
       parent.removeChild(parent.firstChild);
   }
 }
 
+// style properties return empty strings unless you first assign them from javascript
 //https://stackoverflow.com/q/10556185
-const getColor = (item) => {
+const getColor = item => {
   let el = document.querySelector(item);
-  return window.getComputedStyle(el)['backgroundColor'];
+  return window.getComputedStyle(el).backgroundColor;
 }
